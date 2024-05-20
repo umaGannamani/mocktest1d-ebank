69b3796e2a0e380c1ea7149efab3a8d7e5ae159b
uma <udayarekhacowdary96@gmail.com>
2024-05-20 05:58:04 +0000

    # mocktest1d-ebank

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+
+import ProtectedRoute from './components/ProtectedRoute'
+
+import Home from './components/Home'
+
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+// Replace your code here
+const App = () => (
+  <Switch>
+    <Route exact path="/ebank/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
